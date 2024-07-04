178600bd5fb8f1d38b7f8ab7ceec36f255780f7a
MarcoCosta87 <marcoacj018@gmail.com>
2024-07-03 22:03:50 -0300

    projeto bookstoremanager

---
+++ src/test/java/com/projetospring/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospring.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
